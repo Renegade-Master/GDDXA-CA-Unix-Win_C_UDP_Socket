b09d6ebeb41dac3c78c2294e350a37bc72b7f3a9
roryr76 <K00218864@student.lit.ie>
2020-03-09 16:38:47 +0000

    CompileServer/Client_Fork.sh Added

--- hangman/src/hangserver_Fork.c
+++ hangman/src/hangserver_Fork.c
@@ -14,63 +14,7 @@ char* word[] = {
 # define HANGMAN_TCP_PORT 1066
 int maxlives = 12;
 
-/* ---------------- Play_hangman () ---------------------*/
 
-void play_hangman(int in, int out) {
-    char* whole_word, part_word[MAXLEN],
-                      guess[MAXLEN], outbuf[MAXLEN];
-
-    int  lives      = maxlives;
-    int  game_state = 'I';//I = Incomplete
-    int  i, good_guess, word_length;
-    char hostname[MAXLEN];
-
-    gethostname(hostname, MAXLEN);
-    sprintf(outbuf, "Playing hangman on host %s: \n\n", hostname);
-    write(out, outbuf, strlen(outbuf));
-
-    // Pick a word at random from the list
-    whole_word  = word[rand() % NUM_OF_WORDS];
-    word_length = strlen(whole_word);
-    syslog(LOG_USER | LOG_INFO, "Server chose hangman word %s", whole_word);
-
-    // No letters are guessed Initially
-    for (i = 0; i < word_length; i++) {
-        part_word[i] = '-';
-    }
-
-    part_word[i] = '\0';
-
-    sprintf(outbuf, "%s %d \n", part_word, lives);
-    write(out, outbuf, strlen(outbuf));
-
-    while (game_state == 'I')
-        // Get a letter from player guess
-    {
-        while (read(in, guess, MAXLEN) < 0) {
-            if (errno != EINTR) {
-                exit(4);
-            }
-            printf("re-read the start in \n");
-        } // Re-start read () if interrupted by signal/
-        good_guess = 0;
-        for (i     = 0; i < word_length; i++) {
-            if (guess[0] == whole_word[i]) {
-                good_guess = 1;
-                part_word[i] = whole_word[i];
-            }
-        }
-        if (!good_guess) { lives--; }
-        if (strcmp(whole_word, part_word) == 0) {
-            game_state = 'W'; // W ==> User Won
-        } else if (lives == 0) {
-            game_state = 'L'; // L ==> User Lost
-            strcpy(part_word, whole_word); // User Show the word
-        }
-        sprintf(outbuf, "%s %d \n", part_word, lives);
-        write(out, outbuf, strlen(outbuf));
-    }
-}
 
 int main() {
     int                sock, fd, client_len;
@@ -97,11 +41,12 @@ int main() {
 
     while (1) {
         client_len = sizeof(client);
+	//Accept Connection "FORK HERE!!"
         if ((fd    = accept(sock, (struct sockaddr*) &client, &client_len)) < 0) {
             perror("accepting connection");
             exit(3);
         }
-        play_hangman(fd, fd);
+        //play_hangman(fd, fd);
         close(fd);
     }
 }
