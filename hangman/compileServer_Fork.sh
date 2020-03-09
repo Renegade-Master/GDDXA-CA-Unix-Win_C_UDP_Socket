042b8ae2d9c3ce64f6cd0bcc5b412b678415ca45
roryr76 <K00218864@student.lit.ie>
2020-03-09 16:38:47 +0000

    CompileServer/Client_Fork.sh Added

--- hangman/compileServer_Fork.sh
+++ hangman/compileServer_Fork.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run this script to compile the Client code,
+# Run this script to compile the Server code,
 # creating a binary file in the '/bin' folder
 
 # Check for an existing '/bin' folder
@@ -9,9 +9,9 @@ if ! [ -e bin/ ]; then
 fi
 
 # Clear the existing binary files
-if [ -e bin/hangmanClient ]; then
-  rm bin/hangmanClient
+if [ -e bin/hangmanServer ]; then
+  rm bin/hangmanServer_Fork
 fi
 
 # Compile the binary, with WARNINGS replaced with ERRORS
-gcc src/hangclient.c -o bin/hangmanClient -std=gnu99 -Werror
+gcc src/hangserver_Fork.c -o bin/hangmanServer_Fork -std=gnu99 -Werror
